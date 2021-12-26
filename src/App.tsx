2aef9040a249ca9a2afd2f1d79efe961509bfa1f
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-12-26 11:56:09 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
+import '@fontsource/montserrat/700.css'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/300.css'
 import * as React from "react"
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
+  Heading,
+  extendTheme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import { Counter } from './donation/Counter'
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat',
+  },
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
+      <Grid minH="100vh" p={3} bg="gray.50">
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
+          <Logo h="32" pointerEvents="none" />
+          <Heading as="h1" size="xl">JOIN THE MOVEMENT!</Heading>
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+              The team is growing everyday and scoring wins for the planet.
+              <br /> Remove trash with us and track our progress!
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+            <Heading as="h2" size="4xl">
+              <Counter from={0} to={2344535}/>
+            </Heading>
         </VStack>
       </Grid>
     </Box>
--- src/Logo.tsx
+++ src/Logo.tsx
@@ -6,19 +6,8 @@ import {
   forwardRef,
   usePrefersReducedMotion,
 } from "@chakra-ui/react"
-import logo from "./logo.svg"
-
-const spin = keyframes`
-  from { transform: rotate(0deg); }
-  to { transform: rotate(360deg); }
-`
+import logo from "./teamSeasLogo.png"
 
 export const Logo = forwardRef<ImageProps, "img">((props, ref) => {
-  const prefersReducedMotion = usePrefersReducedMotion()
-
-  const animation = prefersReducedMotion
-    ? undefined
-    : `${spin} infinite 20s linear`
-
-  return <chakra.img animation={animation} src={logo} ref={ref} {...props} />
+  return <chakra.img src={logo} ref={ref} {...props} />
 })
