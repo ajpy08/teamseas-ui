7ab2c8eadc76801dc887c0775b298075d175b5ee
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-12-26 14:12:50 -0600

    items from graphql

--- src/leaderboard/Leaderboard.tsx
+++ src/leaderboard/Leaderboard.tsx
@@ -1,36 +1,53 @@
 import { Box, Heading, VStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from 'urql';
+import { Donation } from '../types';
 import { LeaderboardItem } from './LeaderboardItem'
+
+const DonationsQuery = `
+  query Query($orderBy: OrderByParams) {
+    donations(orderBy: $orderBy) {
+      count
+      id
+      displayName
+      createdAt
+      message
+      team
+    }
+  }
+`;
+
+type DonationsQueryRes = {
+  donations: Donation[];
+};
 
 interface Props {
   
 }
 
 export const LeaderBoard = (props: Props) => {
+  const [field, setOrderByField] = useState('createdAt');
+
+  const [{ data, fetching, error }] = useQuery<DonationsQueryRes>({
+    query: DonationsQuery,
+    variables: {
+      orderBy: {
+        field,
+        direction: 'desc',
+      },
+    },
+  });
+
+  if (error) return <p>Something went wrong...</p>;
+  if (fetching || !data) return <p>Loading...</p>;
+
   return (
     <Box w="100%">
-      <Heading>LEADERBOARD</Heading>
       <VStack spacing={4}>
-        <LeaderboardItem donation={{
-          displayName: 'Mr Beast',
-          count: 1000,
-          createdAt: '2021-10-29T17:31:11.795',
-          team: 'team',
-          message: 'some message'
-        }}
-        />
-        <LeaderboardItem donation={{
-          displayName: 'Mr Beast',
-          count: 100,
-          createdAt: '2021-10-29T17:31:11.795'
-        }}
-        />
-        <LeaderboardItem donation={{
-          displayName: 'Mr Beast',
-          count: 1000,
-          createdAt: '2021-10-29T17:31:11.795'
-        }}
-        />
+        <Heading as="h1" size="2xl">LEADERBOARD</Heading>
+        
+        {data.donations.map(donation => <LeaderboardItem donation={donation}
+        />)}
       </VStack>
     </Box>
   )
